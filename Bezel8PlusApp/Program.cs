49d45c67c5abf5d05b94fe70d492516c87603053
jasonlin1111 <tokyo1721@gmail.com>
2019-05-21 17:30:43 +0800

    Revert "Add:"
    This reverts commit 6cc5049fd13a31f75f0b99903502902108fe4802.

--- Bezel8PlusApp/Program.cs
+++ Bezel8PlusApp/Program.cs
@@ -16,7 +16,7 @@ namespace Bezel8PlusApp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new MainEntryForm());
+            Application.Run(new Form1());
         }
     }
 }
